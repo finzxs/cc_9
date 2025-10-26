f37779151608c0c4bef066c21c0f3080d9206a32
Hayden Finney <haydenfinney@usf.edu>
2025-10-26 13:32:46 -0400

    step 3 using fetch with .then and .catch

---
+++ cp_2.js
@@ -0,0 +1,27 @@
+//api docs: https://www.course-api.com/javascript-store-products
+
+const API_URL = 'https://www.course-api.com/javascript-store-products';
+
+
+//step 3 using fetch with .then and .catch
+
+function fetchProductsThen() {
+    fetch(API_URL)
+    .then((res) => {
+        if (!res.ok) throw new Error('HttP ${res.status}');
+        return res.json();
+    })
+    .then((data) => {
+        //logging each product name
+        data.forEach((p, i) => {
+            const name =
+            p?.fiekds?.name ??
+            p?.name ??
+            '(no-name-${1})';
+            console.log(name);
+        });
+    })
+    .catch((err) => {
+        handleError(err);
+    });
+}
